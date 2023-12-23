1a55974481e2b64ec7c90f08c3d9963d87195521
josh <josh>
2023-12-23 16:38:14 +0100

    update Config.js

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@ const chalk = require('chalk')
 
 //contact details
 global.ownernumber =  "2348050907760"
-global.ownername = "king josh"
+global.ownername = "Joshua"
 global.ytname = "YT: JOSHUA.INC"
 global.socialm = "GitHub: Alexmedia23"
 global.location = "Nigeria"
