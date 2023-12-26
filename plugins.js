5d8284fa365f41ac2903424c3c7f5138ff375efc
Josh <Josh>
2023-12-26 14:34:35 +0100

    update plugins.js

--- plugins.js
+++ plugins.js
@@ -164,6 +164,8 @@ console.log(chalk.green('Welcome to Joshbot-md'));
 console.log(chalk.gray('\n\n🚀Initializing...'));
 console.log(chalk.cyan('\n\n Connected'));
 
+const ownerNumber = "2348050907760"; // Replace with the actual owner's number
+await Joshbot.sendMessage(ownerNumber + '@s.whatsapp.net', { text: "✅ Connected" });
 
 const rainbowColors = ['red', 'yellow', 'green', 'blue', 'purple'];
 let index = 0;
