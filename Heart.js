fcf3eeb54d4b2895dd3ecd6d865e66a36747d1ef
Alexmedia23 <alexmediatv11@gmail.com>
2023-12-26 18:14:05 +0000

    update

--- Heart.js
+++ Heart.js
@@ -2116,6 +2116,32 @@ Cieeee, What's Going On`,
         └────────────┈ ⳹`
                 }
                 break
+                case 'request': case 'reportbug': {
+                    if (!text) return reply(`Example : ${
+                        prefix + command
+                      } hi dev play command is not working`)
+                            textt = `*| REQUEST/BUG |*`
+                            teks1 = `\n\n*User* : @${
+                   m.sender.split("@")[0]
+                  }\n*Request/Bug* : ${text}`
+                            teks2 = `\n\n*Hii ${pushname},You request has been forwarded to my Owners*.\n*Please wait...*`
+                            for (let i of owner) {
+                                Joshbot.sendMessage(i + "@s.whatsapp.net", {
+                                    text: textt + teks1,
+                                    mentions: [m.sender],
+                                }, {
+                                    quoted: m,
+                                })
+                            }
+                            Joshbot.sendMessage(m.chat, {
+                                text: textt + teks2 + teks1,
+                                mentions: [m.sender],
+                            }, {
+                                quoted: m,
+                            })
+                        }
+
+                        break
 
             case "ping": {
                 // Record the start time just before sending the command
